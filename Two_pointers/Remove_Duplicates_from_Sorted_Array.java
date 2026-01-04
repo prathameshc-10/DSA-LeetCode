4f48930fae6cc58bcf036fbe347ff284927b755c
Prathamesh Chaudhari <chaudhariprathamesh22@gmail.com>
2026-01-04 12:33:23 +0530

    leetcode_26

---
+++ Two_pointers/Remove_Duplicates_from_Sorted_Array.java
@@ -0,0 +1,30 @@
+package Two_pointers;
+/*
+Pattern: Two Pointers
+
+Approach:
+1. Use two pointers: low for last unique element, high to scan the array
+2. If nums[low] == nums[high], move high
+3. If different, increment low and copy nums[high] to nums[low]
+4. Continue until end of array
+
+Time Complexity: O(n)
+Space Complexity: O(1)
+
+*/
+
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int low = 0;
+        int high = 1;
+        while(high < nums.length){
+            if(nums[low] == nums[high]){
+                high++;
+            } else {
+                low++;
+                nums[low] = nums[high++];
+            }
+        }
+        return low+1;
+    }
+}
